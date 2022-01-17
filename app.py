46589992f46d89bb1ac8b3e734fa8e245a63c942
พิช​ญ์ณิช​ษา​ ขุน​บุตร​ <89174311+Pisnishsa@users.noreply.github.com>
2022-01-17 14:40:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -157,8 +157,8 @@ def event_handle(event):
             replyObj = TextSendMessage(text="ตู่เอง") 
         else : 
             replyObj = TextSendMessage(text=msg) 
-            line_bot_api.reply_message(rtoken, replyObj)
-    else msgType == "image" 
+        line_bot_api.reply_message(rtoken, replyObj)
+    elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
             i = Image.open(BytesIO(message_content.content))
